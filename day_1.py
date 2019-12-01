e15a87990979771c10bf10fdeac71f680493f544
Oleg Kainov <kainov.oleg@gmail.com>
2019-12-01 09:02:28 +0100

    day1: add second part

--- day_1.py
+++ day_1.py
@@ -1,7 +1,21 @@
+def calculate_fuel(mass):
+    total_fuel_mass = 0
+
+    while mass > 0:
+        mass = mass // 3 - 2
+        if mass <= 0:
+            break
+        total_fuel_mass += mass
+    return total_fuel_mass
+
+
 if __name__ == '__main__':
     result = 0
     with open('day_1_input.txt', 'r') as f:
         for line in f:
-            result += int(line) // 3 - 2
+            result += calculate_fuel(int(line))
 
     print(result)
+
+    # First part answer:  3184233
+    # Second part answer: 4773483
