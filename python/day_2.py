1b8804ae052891358ee7e394d64e5c812d8132e4
Oleg Kainov <kainov.oleg@gmail.com>
2019-12-02 08:01:19 +0100

    python: add day 2 part 2

--- python/day_2.py
+++ python/day_2.py
@@ -1,17 +1,21 @@
 import os
 
 
-def solve_day_2_part_1(data):
+def solve_day_2(data, first=12, second=2):
+    # Make explicit copy to do not mess with initial data
+    data = data[::]
+
+    data[1] = first
+    data[2] = second
+
     i = 0
-    data[1] = 12
-    data[2] = 2
     while i < len((data)):
         if data[i] == 99:
             break
-        elif data[i] in [1,2]:
-            first_pos = data[i+1]
+        elif data[i] in [1, 2]:
+            first_pos = data[i + 1]
             second_pos = data[i + 2]
-            result_pos = data[i+3]
+            result_pos = data[i + 3]
             if data[i] == 1:
                 data[result_pos] = data[first_pos] + data[second_pos]
             elif data[i] == 2:
@@ -22,15 +26,17 @@ def solve_day_2_part_1(data):
     return data[0]
 
 
-#print(solve_day_2_part_1([1,1,1,4,99,5,6,0,99]))
-#print(solve_day_2_part_1([1,1,1,4,99,5,6,0,99]))
-
 if __name__ == '__main__':
     result = 0
     with open(os.path.join('..', 'day_2_input.txt'), 'r') as f:
         data = list(map(int, f.read().split(',')))
 
-    print(solve_day_2_part_1(data))
+    print('First part answer: %s' % solve_day_2(data))
+    for i in range(100):
+        for j in range(100):
+            if solve_day_2(data, i, j) == 19690720:
+                print('Second part answer: %s' % str(100 * i + j))
+                break
 
-    # First part answer:  3184233
-    # Second part answer: 4773483
+    # First part answer:  3790689
+    # Second part answer: 6533
