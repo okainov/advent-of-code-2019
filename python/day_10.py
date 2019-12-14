743539db1f29c892c80d7cc1e3f0213ae083ce42
Oleg Kainov <kainov.oleg@gmail.com>
2019-12-14 17:01:49 +0100

    python: add day 10 part 2

--- python/day_10.py
+++ python/day_10.py
@@ -12,15 +12,42 @@ def simplify_delta(delta):
 def calculate_visible(data, x, y):
     data = copy(data)
     start_point = (x, y)
-    deltas = set()
 
+    polar_points = []
     for p in data:
         if p == start_point:
             continue
-        delta = simplify_delta((p[0] - x, p[1] - y))
-        deltas.add(delta)
 
-    return len(deltas)
+        # Coordinates relative to start point
+        relative_x = p[0] - x
+        relative_y = p[1] - y
+        # Convert to polar coordinates
+        r = relative_x * relative_x + relative_y * relative_y
+
+        delta = simplify_delta((relative_x, relative_y))
+
+        if delta[1] == 0:
+            if delta[0] > 0:
+                fi = 90
+            else:
+                fi = 270
+        else:
+            fi = math.atan(delta[0] / delta[1])
+            fi = math.degrees(fi)
+
+            if relative_y < 0:
+                fi += 180
+            elif relative_x < 0:
+                fi += 360
+
+        polar_points.append({
+            'point': p,
+            'delta': delta,
+            'r': r,
+            'fi': fi
+        })
+
+    return polar_points
 
 
 if __name__ == '__main__':
@@ -29,18 +56,45 @@ if __name__ == '__main__':
         for y, line in enumerate(f):
             for x, c in enumerate(line):
                 if c not in ['.', '\n']:
-                    data[(x, y)] = 1
+                    data[(x, -y)] = 1
 
     # O(n)
     max_dst = None
+    station_pos = None
+    good_polar_points = None
     for x, y in data:
         # O(n)
-        curr_dst = calculate_visible(data, x, y)
+        polar_points = calculate_visible(data, x, y)
+        curr_dst = len(set([x['fi'] for x in polar_points]))
         print(f'<{x}, {y}> = {curr_dst}')
         if max_dst is None or curr_dst > max_dst:
             max_dst = curr_dst
+            station_pos = (x, y)
+            good_polar_points = polar_points
 
-    print(max_dst)
+    print(f'Station is placed at {station_pos} == {max_dst}')
+
+    sort = sorted(good_polar_points, key=lambda x: (x['fi'], x['r']))
+
+    n_total = len(sort)
+    vaporization = []
+    i = 0
+    while len(vaporization) < n_total:
+        vaporize = sort[i]
+        vaporization.append(vaporize)
+        sort.remove(vaporize)
+        if not sort:
+            break
+        i = i % len(sort)
+
+        if len(set([x['fi'] for x in sort])) == 1:
+            # Finish stage, only asteroids on one line left, just vaporize them in order
+            continue
+
+        while vaporize['fi'] == sort[i]['fi']:
+            i = (i + 1) % len(sort)
+
+    print(vaporization[199]['point'][0] * 100 - vaporization[199]['point'][1])
 
     # First part answer:  256
     # Second part answer:
