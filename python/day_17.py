b79031634669799a810d23ec637763b6a1fea868
Oleg Kainov <kainov.oleg@gmail.com>
2019-12-17 20:07:34 +0100

    python: add day 17 part 2
    Cannot believe it was so simple!!!!

--- python/day_17.py
+++ python/day_17.py
@@ -26,19 +26,23 @@ class Arcade:
         x = 0
         y = 0
         for i in output_buffer:
-            if i == 35:
-                # Scaffold
-                self.state[(x, y)] = 1
-                x += 1
-            elif i == 46:
-                self.state[(x, y)] = '.'
-                x += 1
-            elif i == 10:
+            if i > 200:
+                # Assuming it's a final score
+                print(f'Part 2 final score: {i}')
+                continue
+            if i == 10:
                 # Newline
                 y += 1
                 x = 0
-            else:
-                x += 1
+                continue
+            self.state[(x, y)] = chr(i)
+            x += 1
+
+    def provide_text_input(self, string):
+        for c in string:
+            self.vm.add_input(ord(c))
+
+        self.vm.add_input(ord('\n'))
 
     def play(self):
         while True:
@@ -47,29 +51,42 @@ class Arcade:
                 self.vm.tick()
 
             except InputNeededException:
-
-                print_image(self.state)
-
+                os.system('cls')
+                print('')
                 self.handle_outputs()
-                # self.vm.add_input(int(input_value))
+
+                print_image(self.state, interpret_as_ascii=True)
+
+                # Full R6 L10 R8 R8 R12 L8 L10 R6 L10 R8 R8 R12 L10 R6 L10 R12 L8 L10 R12 L10 R6 L10 R6 L10 R8 R8 R12 L8 L10 R6 L10 R8 R8 R12 L10 R6 L10
+                # Just remap the values:
+                # D E F F G K E D E F F G E D E G K E G E D E D E F F G K E D E F F G E D E
+
+                # D = R6
+                # E = L10
+                # F = R8
+                # G = R12
+                # K = L8
+
+                # D E F F | G K E | D E F F | G E D E | G K E | G E D E | D E F F | G K E | D E F F | G E D E
+                # =>
+                # A = GKE = R12 L8 L10
+                # B = DEFF = R6 L10 R8 R8
+                # C = GEDE = R12 L10 R6 L10
+
+                self.provide_text_input('B,A,B,C,A,C,B,A,B,C')
+                self.provide_text_input('R,12,L,8,L,10')
+                self.provide_text_input('R,6,L,10,R,8,R,8')
+                self.provide_text_input('R,12,L,10,R,6,L,10')
+                self.provide_text_input('n')
                 continue
 
             except InterruptedError:
+                os.system('cls')
+                print('')
                 self.handle_outputs()
-                print_image(self.state)
+                print_image(self.state, interpret_as_ascii=True)
 
-                result = 0
-                states_copy = copy.deepcopy(self.state)
-                for coord in states_copy:
-                    if self.state[coord] == 1 and self.state[sum_pairs(coord, (0, 1))] == 1 and \
-                            self.state[sum_pairs(coord, (0, -1))] == 1 and \
-                            self.state[sum_pairs(coord, (1, 0))] == 1 and \
-                            self.state[sum_pairs(coord, (-1, 0))] == 1:
-                        result += coord[0] * coord[1]
-                        print(coord[0] * coord[1])
-                print(f'Part 1: {result}')
-
-                print('GAME OVER!')
+                print('\nGAME OVER!')
 
                 break
 
@@ -77,8 +94,8 @@ class Arcade:
 if __name__ == '__main__':
     with open(os.path.join('..', 'day_17_input.txt'), 'r') as f:
         data = list(map(int, f.read().split(',')))
-    automat = Arcade(data, n_quarters=1)
+    automat = Arcade(data, n_quarters=2)
     automat.play()
 
     # First part answer:  8408
-    # Second part answer: 12765
+    # Second part answer: 1168948
--- python/utils.py
+++ python/utils.py
@@ -6,7 +6,7 @@ def substract_pairs(pair_1, pair_2):
     return pair_1[0] - pair_2[0], pair_1[1] - pair_2[1]
 
 
-def print_image(image, reversed_y=False):
+def print_image(image, reversed_y=False, interpret_as_ascii=False):
     height = max(image.keys(), key=lambda x: x[1])[1] + 1
     height_start = min(image.keys(), key=lambda x: x[1])[1]
     width = max(image.keys(), key=lambda x: x[0])[0] + 1
@@ -19,17 +19,20 @@ def print_image(image, reversed_y=False):
         # Print newline
         print('')
         for x in range(width_start, width):
-            char = ' '
-            if image[(x, y)] in [1, '1']:
-                # Wall
-                char = chr(0x2591)
-            elif image[(x, y)] in [2, '2']:
-                # Block
-                char = chr(0x2588)
-            elif image[(x, y)] in [3, '3']:
-                char = chr(0x2588)
-                # Print paddle twice
-                print( chr(0x2588), end='')
-            elif image[(x, y)] in [4, '4']:
-                char = 'o'
-            print(char, end='')
+            if interpret_as_ascii:
+                print(image[(x, y)], end='')
+            else:
+                char = '.'
+                if image[(x, y)] in [1, '1']:
+                    # Wall
+                    char = chr(0x2591)
+                elif image[(x, y)] in [2, '2']:
+                    # Block
+                    char = chr(0x2588)
+                elif image[(x, y)] in [3, '3']:
+                    char = chr(0x2588)
+                    # Print paddle twice
+                    # print( chr(0x2588), end='')
+                elif image[(x, y)] in [4, '4']:
+                    char = 'o'
+                print(char, end='')
